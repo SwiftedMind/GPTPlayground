e8200b43b77ce6ff1fc7ec349c77c94c177b5668
Dennis Müller <d3mueller@me.com>
2023-03-03 19:09:33 +0100

    Initial Commit

---
+++ GPTPlayground/GPTPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPTPlaygroundApp.swift
+//  GPTPlayground
+//
+//  Created by Dennis Müller on 03.03.23.
+//
+
+import SwiftUI
+
+@main
+struct GPTPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
